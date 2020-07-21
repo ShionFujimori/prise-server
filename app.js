48086519b06668d8b88bb73f0017fade24d124cc
ShionFujimori <shion888@gmail.com>
2020-07-21 14:53:44 +0900

    Update DB and CORS settings

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
+// load modules
 const express = require('express');
 const mysql = require('mysql');
 
+// create an instance of the express module
 const app = express();
 
-const connection = mysql.createConnection({
+// mysql connection info
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'teamprise',
     password: 'prise2020',
-    database: 'user'
+    database: 'prise'
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('success');
+// CORS (Cross-origin resource sharing)
+// A protocol that enables scripts running on a browser client
+// to interact with resources from a different origin
+app.use((req, res, next)　=> {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, \
+              Content-Type, Accept");
+  next();
 });
 
+// '/' routing
 app.get('/', (req, res) => {
     res.render('top.ejs');
-})
+});
 
-app.listen(5000);
+// '/posts' routing
+app.get('/posts', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) throw err;
+        connection.query(
+            'SELECT * FROM user',
+            (error, results) => {
+                res.send(results);
+                connection.release();
+            }
+        );
+    });
+});
+
+// Run a local web server (localhost:4000)
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
